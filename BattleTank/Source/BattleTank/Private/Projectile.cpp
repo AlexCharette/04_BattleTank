8aa0ebda4c2a9cc91cc54577373861b486635bfb
Alexander Charette <acharette.wake@gmail.com>
2016-07-03 15:17:31 -0400

    Create projectile C++ class.

---
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "Projectile.h"
+
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ProjectileMovingComponent = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Moving Component"));
+	ProjectileMovingComponent->bAutoActivate = false;
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectile::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void AProjectile::Launch(float speed)
+{
+	ProjectileMovingComponent->SetVelocityInLocalSpace(FVector::ForwardVector * speed); // forward vector is a unit vector
+	ProjectileMovingComponent->Activate();
+}
+
