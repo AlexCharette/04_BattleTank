582661d9e1d309a8c05f33704150516051331a07
Alexander Charette <acharette.wake@gmail.com>
2016-08-15 10:37:39 -0400

    Implement HealthBar and BlueprintPure GetHealthPercent function on the Tank.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -14,9 +14,13 @@ public:
 	// Called by the engine when Actor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const &DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 
+	// Return current health as percentage of starting health (0 - 1)
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
-	int32 StartingHealth = 60;
+	int32 StartingHealth = 100;
 
 	UPROPERTY(VisibleAnywhere, Category = "Health")
 	int32 CurrentHealth = StartingHealth;
