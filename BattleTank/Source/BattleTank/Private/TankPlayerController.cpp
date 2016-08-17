d1350a2a9ee01765172016da14a2896152978d34
Alexander Charette <acharette.wake@gmail.com>
2016-08-17 13:56:20 -0400

    Add First Person Camera and swapping support.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -79,7 +79,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVec
 			Hit,
 			StartLocation,
 			EndLocation,
-			ECollisionChannel::ECC_Visibility)
+			ECollisionChannel::ECC_Camera)
 		)
 	{
 		HitLocation = Hit.Location;
